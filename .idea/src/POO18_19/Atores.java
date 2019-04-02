5611111e372203983c5b90e94abd6eec509647a5
Berneli0 <47863833+Berneli0@users.noreply.github.com>
2019-04-02 15:32:31 +0100

    Classe Atores terminada
    (penso eu...)
    Só falta alugueres, ainda tenho de perceber como é q essa funciona

--- .idea/src/POO18_19/Atores.java
+++ .idea/src/POO18_19/Atores.java
@@ -29,6 +29,26 @@ public class Atores
         return this.dataNascimento;
     }
     
+    public void setEmail (String e) {
+        this.email = e;
+    }
+    
+    public void setNome (String n) {
+        this.nome = n;
+    }
+    
+    public void setPassword (String p) {
+        this.password = p;
+    }
+    
+    public void setMorada (String m) {
+        this.morada = m;
+    }
+    
+    public void setDataNascimento (String d) {
+        this.dataNascimento = d;
+    }
+    
  public class Cliente extends Atores 
  {
    private Pair<Double,Double> localizacao;
@@ -75,6 +95,131 @@ public class Atores
        for(Aluguer s : this.historial)
             a.add(s);
        return a;
-    }
+   }
+   
+   public void setLocalizacao (Pair<Double,Double> l) {
+       this.localizacao = l;
+   }
+   
+   public void setHistorial (List<Aluguer> h) {
+       this.historial = new ArrayList<>();
+       for(Aluguer a : h)
+            this.historial.add(a);
+   }
+   
+   public Cliente clone() {
+       return new Cliente();
+   }
+   
+   public boolean equals (Object o) {
+       if(o == this) return true;
+       if(o == null || o.getClass() != this.getClass()) return false;
+       Cliente c = (Cliente) o;
+       return c.getEmail().equals(this.email) &&
+              c.getNome().equals(this.nome) && 
+              c.getPassword().equals(this.password) &&
+              c.getMorada().equals(this.morada) &&
+              c.getDataNascimento().equals(this.dataNascimento) &&
+              c.getLocalizacao().equals(this.localizacao) &&
+              c.getHistorial().equals(this.historial);
+   }
+   
+   public String toString() {
+       StringBuilder st = new StringBuilder();
+       st.append("Dados do cliente: \n").append("Email: ").append(this.email);
+       st.append(", nome: ").append(this.nome).append(", password: ").append(this.password);
+       st.append(", morada: ").append(this.morada).append(", data de nascimento: ").append(this.dataNascimento);
+       st.append(", localização: ").append(this.localizacao).append(", historial de alugueres: ");
+       for(Aluguer a : this.historial)
+            st.append(a);
+       return st.toString();
+   }
+ } 
+ public class Proprietario extends Atores 
+ {
+     private int classificacao;
+     private List<Aluguer> historico;
+     
+   public Proprietario () {
+       this.email = "n/a";
+       this.nome = "n/a";
+       this.password = "n/a";
+       this.morada = "n/a";
+       this.dataNascimento = "n/a";
+       this.classificacao = 0;
+       this.historico = new ArrayList<>();
+   }
+   
+   public Proprietario (String e, String n, String p, String m, String d, int c, List<Aluguer> h) {
+       this.email = e;
+       this.nome = n;
+       this.password = p;
+       this.morada = m;
+       this.dataNascimento = d;
+       this.classificacao = c;
+       this.historico = new ArrayList<Aluguer>();
+       for(Aluguer a : h)
+            this.historico.add(a);
+   }
+   
+   public Proprietario (Proprietario umProprietario) {
+       this.email = umProprietario.getEmail();
+       this.nome = umProprietario.getNome();
+       this.password = umProprietario.getPassword();
+       this.morada = umProprietario.getMorada();
+       this.dataNascimento = umProprietario.getDataNascimento();
+       this.classificacao = umProprietario.getClassificacao();
+       this.historico = umProprietario.getHistorico();
+   }
+   
+   
+   public int getClassificacao() {
+       return this.classificacao;
+   }
+   
+   public List<Aluguer> getHistorico() {
+       ArrayList<Aluguer> a = new ArrayList<>();
+       for(Aluguer s : this.historico)
+            a.add(s);
+       return a;
+   }
+   
+   public void setClassificacao (int c) {
+       this.classificacao = c;
+   }
+   
+   public void setHistorico (List<Aluguer> h) {
+       this.historico = new ArrayList<>();
+       for(Aluguer a : h)
+            this.historico.add(a);
+   }
+   
+   public Proprietario clone() {
+       return new Proprietario();
+   }
+   
+   public boolean equals (Object o) {
+       if(o == this) return true;
+       if(o == null || o.getClass() != this.getClass()) return false;
+       Proprietario p = (Proprietario) o;
+       return p.getEmail().equals(this.email) &&
+              p.getNome().equals(this.nome) && 
+              p.getPassword().equals(this.password) &&
+              p.getMorada().equals(this.morada) &&
+              p.getDataNascimento().equals(this.dataNascimento) &&
+              p.getClassificacao() == this.classificacao &&
+              p.getHistorico().equals(this.historico);
+   }
+   
+   public String toString() {
+       StringBuilder st = new StringBuilder();
+       st.append("Dados do proprietário: \n").append("Email: ").append(this.email);
+       st.append(", nome: ").append(this.nome).append(", password: ").append(this.password);
+       st.append(", morada: ").append(this.morada).append(", data de nascimento: ").append(this.dataNascimento);
+       st.append(", classificação: ").append(this.classificacao).append(", historial de alugueres: ");
+       for(Aluguer a : this.historico)
+            st.append(a);
+       return st.toString();
+   }
  }
 }
