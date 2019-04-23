8fb684322998b3955f924192d0499e82578a9b70
Pedro Oliveira <47835317+pedromgdo@users.noreply.github.com>
2019-04-23 15:06:58 +0100

    Small changes things are ready to be worked on
    yeet

--- src/master/Main.java
+++ src/master/Main.java
@@ -3,16 +3,7 @@ package master;
 public class Main{
     public static void main(String[] args){
         System.out.println("Start of main.");
-        Friend pedro = new Friend();
-        pedro.Friend(19,180,10);
-        System.out.println("Isto é um teste, o pedro tem " + pedro.idade + " de idade.");
-        System.out.println("Isto é um teste, o pedro tem " + pedro.altura + " de altura.");
-        System.out.println("Isto é um teste, o pedro tem " + pedro.dinheiro + " de dinheiro.");
-
-        Potato batata = new Potato(5,"Semilha");
-
-        System.out.println("Isto é um teste, a " + batata.nome + " tem de peso: " + batata.peso);
-
+        Cliente pedro = new Cliente();
         System.out.println("End of main.");
     }
 }
