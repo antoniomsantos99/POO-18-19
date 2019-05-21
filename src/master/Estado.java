cb1d749872bcf3539c5d93bf94632e579c104a6f
Pedro Oliveira <47835317+pedromgdo@users.noreply.github.com>
2019-05-21 13:26:51 +0100

    Estado Severamente Alterado, can't back down now :/

--- src/master/Estado.java
+++ src/master/Estado.java
@@ -20,108 +20,155 @@ public class Estado {
      */
     private final String path = "saves/";
 
-    private Map<String,Cliente> listaClientes;
-    private Map<String,Proprietario> listaProprietarios;
-    private Map<String,Carro> listaCarros;
+    private Map<String, Cliente> listaClientes;
+    private Map<String, Proprietario> listaProprietarios;
+    private Map<String, Carro> listaCarros;
 
-    public Estado(){
+    private Menu menuLogin;
+    private Menu menuRegisto;
+
+    public Estado() {
+        String[] opcoesLogin = {"Login Cliente","Login Proprietario"};
+        String[] opcoesRegisto = {"Registo Cliente","Registo Proprietario"};
+        this.menuLogin = new Menu(opcoesLogin);
+        this.menuRegisto = new Menu(opcoesRegisto);
+
         this.listaClientes = new HashMap<>();
         this.listaProprietarios = new HashMap<>();
         this.listaCarros = new HashMap<>();
     }
 
-    public Estado(Map<String,Cliente> listaClientes, Map<String,Proprietario> listaProprietarios, Map<String,Carro> listaCarros){
-        this.listaClientes = listaClientes.entrySet().stream().collect(Collectors.toMap(e->e.getKey(),e->e.getValue().clone()));
-        this.listaProprietarios = listaProprietarios.entrySet().stream().collect(Collectors.toMap(e->e.getKey(),e->e.getValue().clone()));
-        this.listaCarros = listaCarros.entrySet().stream().collect(Collectors.toMap(e->e.getKey(),e->e.getValue().clone()));
+    public Estado(Map<String, Cliente> listaClientes, Map<String, Proprietario> listaProprietarios, Map<String, Carro> listaCarros) {
+        String[] opcoesLogin = {"Login Cliente","Login Proprietario"};
+        String[] opcoesRegisto = {"Registo Cliente","Registo Proprietario"};
+        this.menuLogin = new Menu(opcoesLogin);
+        this.menuRegisto = new Menu(opcoesRegisto);
+
+        this.listaClientes = listaClientes.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().clone()));
+        this.listaProprietarios = listaProprietarios.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().clone()));
+        this.listaCarros = listaCarros.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().clone()));
     }
 
     /**
      * metodo estatico para adicionar clientes ao programa
+     *
      * @param cliente recebe um cliente
      */
-    public void addCliente(Cliente cliente){
-        listaClientes.put(cliente.getEmail(),cliente);
+    public void addCliente(Cliente cliente) {
+        listaClientes.put(cliente.getEmail(), cliente);
     }
 
     /**
      * metodo estatico para adicionar proprietarios ao programa
+     *
      * @param proprietario recebe um proprietario
      */
-    public void addProprietario(Proprietario proprietario){
-        listaProprietarios.put(proprietario.getEmail(),proprietario);
+    public void addProprietario(Proprietario proprietario) {
+        listaProprietarios.put(proprietario.getEmail(), proprietario);
     }
 
     /**
      * por fazer mas:
      * metodo estatico para adicionar um carro ao programa
+     *
      * @param carro carro
      */
-    public void addCarro(Carro carro){
-        listaCarros.put(carro.matricula,carro);
+    public void addCarro(Carro carro) {
+        listaCarros.put(carro.matricula, carro);
     }
 
     /**
      * verifica se um cliente com um nome ou email existe no sistema
+     *
      * @param email recebe o mail
      * @return verdadeiro se existir, falso se não
      */
-    public boolean verificaCliente(String email){
+    public boolean verificaCliente(String email) {
         return listaClientes.containsKey(email);
     }
 
     /**
      * verifica se um proprietario com um nome ou email existe no sistema
+     *
      * @param email recebe o mail
      * @return verdadeiro se existir, falso se não
      */
-    public boolean verificaProprietario(String email){
+    public boolean verificaProprietario(String email) {
         return listaProprietarios.containsKey(email);
+    }
+
+    public void login(){
+        do{
+            menuLogin.executa();
+            switch(menuLogin.getOpcao()){
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+        }while(menuLogin.getOpcao()!=0);
+    }
+    public void registo(){
+        do{
+            menuRegisto.executa();
+            switch(menuRegisto.getOpcao()){
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+        }while(menuRegisto.getOpcao()<0);
+        System.out.println("Registo Efetuado com sucesso!");
     }
 
     /**
      * tenta fazer o login com um email e uma password
-     * @param email email
+     *
+     * @param email    email
      * @param password password
      * @return verdadeiro se conseguir falso se não
      */
-    public boolean tryLoginCliente(String email, String password){
+    private boolean tryLoginCliente(String email, String password) {
         Cliente c = listaClientes.get(email);
         return c.getPassword().equals(password);
     }
 
     /**
      * tenta fazer o login com um email e uma password
-     * @param email username ou email
+     *
+     * @param email    username ou email
      * @param password password
      * @return verdadeiro se conseguir falso se não
      */
-    public boolean tryLoginProprietario(String email, String password){
+    private boolean tryLoginProprietario(String email, String password) {
         Proprietario p = listaProprietarios.get(email);
         return p.getPassword().equals(password);
     }
 
-
-    public Cliente getCliente(String email){
+    public Cliente getCliente(String email) {
         return listaClientes.get(email);
     }
-    public Proprietario getProprietario(String email){
+
+    public Proprietario getProprietario(String email) {
         return listaProprietarios.get(email);
     }
-    public Carro getCarro(String matricula){return listaCarros.get(matricula);}
 
-    public void carregarEstado(){
-        String[] opcoes = {"Carregar Rápido","Carregar com Nome do ficheiro"};
+    public Carro getCarro(String matricula) {
+        return listaCarros.get(matricula);
+    }
+
+    public void carregarEstadoTXT() {
+        String[] opcoes = {"Carregar Rápido", "Carregar com Nome do ficheiro"};
         Menu m = new Menu(opcoes);
-        do{
+        do {
             m.executa();
-            switch(m.getOpcao()){
+            switch (m.getOpcao()) {
                 case 1:
-                    try{
-                        lerCVC(path+"state.txt");
-                    }catch(FileNotFoundException e){
+                    try {
+                        lerFile(path + "state.txt");
+                    } catch (FileNotFoundException e) {
                         System.out.println("Erro: Não foi possivel ler o ficheiro.");
-                    }catch(IOException io){
+                    } catch (IOException io) {
                         System.out.println("Erro: Problema ao tentar ler");
                     }
                     break;
@@ -129,28 +176,29 @@ public class Estado {
                     Scanner is = new Scanner(System.in);
                     System.out.print("Nome do Ficheiro que pretende carregar (.txt): ");
                     String nomeFile = is.nextLine();
-                    try{
-                        lerCVC(path+nomeFile);
-                    }catch(FileNotFoundException e){
+                    try {
+                        lerFile(path + nomeFile);
+                    } catch (FileNotFoundException e) {
                         System.out.println("Erro: Não foi possivel ler o ficheiro.");
-                    }catch(IOException io){
+                    } catch (IOException io) {
                         System.out.println("Erro: Problema ao tentar ler");
                     }
                     break;
             }
-        }while(m.getOpcao()<0);
+        } while (m.getOpcao() < 0);
 
     }
-    public void gravarEstado(){
-        String[] opcoes = {"Gravar Rápido","Gravar com Nome do ficheiro"};
+
+    public void gravarEstadoTXT() {
+        String[] opcoes = {"Gravar Rápido", "Gravar com Nome do ficheiro"};
         Menu m = new Menu(opcoes);
-        do{
+        do {
             m.executa();
-            switch(m.getOpcao()){
+            switch (m.getOpcao()) {
                 case 1:
-                    try{
-                        escreverFile(path+"state.txt");
-                    }catch(FileNotFoundException e){
+                    try {
+                        escreverFile(path + "state.txt");
+                    } catch (FileNotFoundException e) {
                         System.out.println("Erro: Não foi possivel guardar o ficheiro.");
                     }
                     break;
@@ -158,61 +206,91 @@ public class Estado {
                     Scanner is = new Scanner(System.in);
                     System.out.print("Nome do Ficheiro que pretende gravar (.txt): ");
                     String nomeFile = is.nextLine();
-                    try{
-                        escreverFile(path+nomeFile);
-                    }catch(FileNotFoundException e){
+                    try {
+                        escreverFile(path + nomeFile);
+                    } catch (FileNotFoundException e) {
                         System.out.println("Erro: Não foi possivel guardar o ficheiro.");
                     }
                     break;
             }
-        }while(m.getOpcao()<0);
+        } while (m.getOpcao() < 0);
 
     }
 
     private void escreverFile(String file) throws FileNotFoundException {
 
         PrintWriter fich = new PrintWriter(file);
-        for(Cliente c: this.listaClientes.values())
+        for (Cliente c : this.listaClientes.values())
             fich.println(c.toString());
-        for(Proprietario p: this.listaProprietarios.values())
+        for (Proprietario p : this.listaProprietarios.values())
             fich.println(p.toString());
-        for(Carro c: this.listaCarros.values())
+        for (Carro c : this.listaCarros.values())
             fich.println(c.toString());
 
         fich.flush();
         fich.close();
     }
 
-    private void lerCVC(String file) throws FileNotFoundException, IOException {
+    private void lerFile(String file) throws FileNotFoundException, IOException {
         List<String> linhas = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(file));
         String linha;
 
-        while( (linha = br.readLine()) != null ) {
+        while ((linha = br.readLine()) != null) {
             linhas.add(linha);
         }
         br.close();
         System.out.println(linhas);
 
         linhas.forEach(s -> {
-            if(s.equals("NovoProp:")) CVC2Proprietario(s);
-            if(s.equals("NovoCliente:")) CVC2Cliente(s);
-            if(s.equals("NovoCarro:")) CVC2Carro(s);
+            if (s.equals("NovoProp:")) CVC2Proprietario(s);
+            if (s.equals("NovoCliente:")) CVC2Cliente(s);
+            if (s.equals("NovoCarro:")) CVC2Carro(s);
         });
     }
 
     //TODO
-    private void CVC2Cliente(String linha){
+    private void CVC2Cliente(String linha) {
         Cliente c = null;
 
     }
-    private void CVC2Proprietario(String linha){
+
+    private void CVC2Proprietario(String linha) {
         Proprietario p = null;
 
     }
-    private void CVC2Carro(String linha){
+
+    private void CVC2Carro(String linha) {
         Carro c = null;
 
+    }
+    /**
+     * Método que guarda em ficheiro de objectos o objecto que recebe a mensagem.
+     */
+    public void guardaEstado(String nomeFicheiro)
+            throws IOException {
+        FileOutputStream fos = new FileOutputStream(nomeFicheiro);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(this); //guarda-se o objecto de uma só vez
+        oos.flush();
+        oos.close();
+    }
+    /**
+     * Método que recupera uma instância de Estado de um ficheiro de objectos.
+     * Este método tem de ser um método de classe que devolva uma instância já
+     * construída de Estado.
+     *
+     * @param nomeFicheiro do ficheiro onde está guardado um objecto do tipo Estado
+     * @return objecto Estado inicializado
+     */
+
+    public static Estado carregaEstado(String nomeFicheiro)
+            throws FileNotFoundException,IOException,ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(nomeFicheiro);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Estado e = (Estado) ois.readObject();
+        ois.close();
+        return e;
     }
 
     //PARA DEBUG APENAS, DEPOIS REMOVER
--- src/master/UMCarroJa.java
+++ src/master/UMCarroJa.java
@@ -1,10 +1,15 @@
 package master;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class UMCarroJa{
 
-    private static Estado estado;
+    private Estado estado;
 
     private Menu menuPrincipal;
+
+    private Menu menuLogOuReg;
 
     public static void main(String[] args){
         System.out.println("Start of main.");
@@ -16,30 +21,61 @@ public class UMCarroJa{
     }
 
     public UMCarroJa(){
-        String[] opcoes = {"Novo Estado","Carregar Estado","Gravar Estado"};
-        estado = new Estado();
-        this.menuPrincipal = new Menu(opcoes);
+        try{
+            this.estado = Estado.carregaEstado("estado.obj");
+        }catch(FileNotFoundException e){
+            System.out.println("Primeira execução?");
+            this.estado = new Estado();
+        }catch(IOException e){
+            System.out.println("Erro ao tentar ler Estado.");
+            this.estado = new Estado();
+        }catch(ClassNotFoundException e){
+            System.out.println("Formato do ficheiro de dados errado.");
+            this.estado = new Estado();
+        }
+        String[] opcoesMenuPrincipal = {"Continuar ->","Carregar Estado (.txt)","Gravar Estado (.txt)"};
+        String[] opcoesLogOuReg = {"Login","Registo"};
+        this.menuPrincipal = new Menu(opcoesMenuPrincipal);
+        this.menuLogOuReg = new Menu(opcoesLogOuReg);
     }
 
     private void run(){
         do{
             menuPrincipal.executa();
-
             switch (menuPrincipal.getOpcao()){
                 case 1:
-                    new LoginRegisto().run(estado);
-                    break;
+                    loginOuRegisto();
                 case 2:
-                    estado.carregarEstado();
-                    new LoginRegisto().run(estado);
+                    //WIP
+                    estado.carregarEstadoTXT();
                     //System.out.println(estado.debugString());
                     break;
                 case 3:
-                    estado.gravarEstado();
+                    //WIP
+                    estado.gravarEstadoTXT();
                     //System.out.println(estado.debugString());
                     break;
             }
         }while(menuPrincipal.getOpcao()!=0);
+        try{
+            this.estado.guardaEstado("estado.obj");
+        }catch(IOException e){
+            System.out.println("Erro ao tentar gravar Estado.");
+        }
+    }
+
+    private void loginOuRegisto(){
+        do{
+            menuLogOuReg.executa();
+            switch(menuLogOuReg.getOpcao()){
+                case 1:
+                    this.estado.login();
+                    break;
+                case 2:
+                    this.estado.registo();
+                    break;
+            }
+        }while(menuLogOuReg.getOpcao()!=0);
     }
 
 }
